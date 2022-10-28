c7d45ce53f158815ac1d3de0d6f02095a5dccedc
Agustin Salum <agustinsalum92@hotmail.com>
2022-10-27 23:55:18 -0300

    Modificado archivo GemCountable.gamspec y agregada la funcionalidad en lib

--- GemCountable.gemspec
+++ GemCountable.gemspec
@@ -8,17 +8,16 @@ Gem::Specification.new do |spec|
   spec.authors = ["Agustin Salum"]
   spec.email = ["agustinsalum92@hotmail.com"]
 
-  spec.summary = "TODO: Write a short summary, because RubyGems requires one."
-  spec.description = "TODO: Write a longer description or delete this line."
-  spec.homepage = "TODO: Put your gem's website or public repo URL here."
+  spec.summary = "Permite que cualquier clase cuente la cantidad de veces que los metodos de instancia definidos en ella es invocado"
+  spec.homepage = "https://github.com/agustinsalum"
   spec.license = "MIT"
-  spec.required_ruby_version = ">= 2.6.0"
+  spec.required_ruby_version = ">= 3.1.2"
 
   spec.metadata["allowed_push_host"] = "TODO: Set to your gem server 'https://example.com'"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = "https://github.com/agustinsalum/GemCountable"
+  spec.metadata["changelog_uri"] = "https://github.com/agustinsalum/GemCountable"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
--- lib/GemCountable.rb
+++ lib/GemCountable.rb
@@ -3,6 +3,37 @@
 require_relative "GemCountable/version"
 
 module GemCountable
-  class Error < StandardError; end
-  # Your code goes here...
+  module Countable
+    module CountableMethodClass
+      # método de clase que al invocarse realiza las tareas necesarias para contabilizar las invocaciones al método de instancia cuyo nombre es sym (un símbolo).
+      def count_invocations_of(sym)
+        alias_method "#{sym}_copia", sym
+        define_method(sym)  do
+          # El self(metodo sym) no es obligatorio con define_method
+          self.contabilizar[sym]+=1
+          self.send("#{sym}_copia")
+        end
+      end
+    end
+    
+    # Cuando un modulo es incluido en una clase se ejecuta este metodo (no es metodo de clase)
+    def self.included(base)
+      base.extend(CountableMethodClass)
+    end
+    
+    def contabilizar
+      # ||=  si el hash esta vacio entonces retorna false... true en caso contrario
+      @d||=Hash.new(0) # Las nuevas keys van a iniciarse con un value en cero, usado como contador
+    end
+    
+    # método de instancia que devuelve un valor booleano indicando si el método llamado sym fue invocado al menos una vez en la instancia receptora.
+    def invoked?(sym)
+      @d[sym] > 0
+    end
+    
+    # método de instancia que devuelve la cantidad de veces que el método identificado por sym fue invocado en la instancia receptora.
+    def invoked(sym)
+      @d[sym]
+    end
+  end
 end
